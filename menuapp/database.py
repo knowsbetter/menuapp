a5c75ac82873a789fa0a46e202b223e7f4e5d655
knowsbetter <alexz87@mail.ru>
2023-01-29 01:13:13 +0500

    Update database.py

--- menuapp/database.py
+++ menuapp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@db:5432/postgres'  # Docker database
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:breakme@localhost:5432/my_menu'  # Local database
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/my_menu'  # Local database
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
